8cf71e1c27ce80299f9e66374d8608099f64091f
natttydp <2014110767@ub.edu.bz>
2025-01-25 14:40:04 -0600

    Finished Question One

---
+++ questionOne.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare and Initialize an array of size 5
+    int numbers[5] = {1, 2, 3, 4, 5};
+    
+    // Print the array elements
+    for(int i = 0; i < 5; i++) {
+        cout << numbers[i] << " ";
+    }
+
+    return 0;
+}
